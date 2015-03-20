ad2da18bb201b13a33a997a6ecb9b6d1242dae70
Jill Kuchman <jillkuchman@gmail.com>
2015-03-20 11:59:14 -0700

    Add app.php skeleton file

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app = new Silex\Application();
+
+    $app['debug']=true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $DB = new PDO('pgsql:host=localhost;dbname=hair_salon');
+
+    return $app;
+
+?>
