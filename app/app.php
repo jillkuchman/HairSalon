5700410ca0009e881edab5a44afe64c221c7bc72
Jill Kuchman <jillkuchman@gmail.com>
2015-03-20 13:41:01 -0700

    Add display framework (stylist name, for loop to display list of clients, form to add new client) to stylist.html.twig

--- app/app.php
+++ app/app.php
@@ -25,7 +25,7 @@
     });
 
     $app->post("/stylists", function() use ($app) {
-        $new_stylist = new Stylist($id=null, $_POST['stylist_name']);
+        $new_stylist = new Stylist($id=null,  $_POST['stylist_name']);
         $new_stylist->save();
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
