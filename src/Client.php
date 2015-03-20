8cd004740b75c3ea508cdc0bee560fedc68b7c56
Jill Kuchman <jillkuchman@gmail.com>
2015-03-20 09:24:19 -0700

    Add constructor for Client class

--- src/Client.php
+++ src/Client.php
@@ -4,7 +4,15 @@
     {
         private $id;
         private $name;
-        
+
+        function __construct ($id = null, $name)
+        {
+            if($id !== null) {
+                $this->id = $id;
+            }
+            $this->name = $name;
+        }
+
     }
 
 ?>
